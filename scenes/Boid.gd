6009174233ddcd462a6a19ffc6b9128a6314d4e0
ethanernst <ethanernst1@gmail.com>
2024-01-07 20:24:14 -0500

    base godot config

---
+++ scenes/Boid.gd
@@ -0,0 +1,17 @@
+extends Sprite2D
+
+@export_range(0, 1) var separation
+@export_range(0, 1) var alignment
+@export_range(0, 1) var cohesion
+
+var direction = Vector2()
+var velocity = Vector2()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
