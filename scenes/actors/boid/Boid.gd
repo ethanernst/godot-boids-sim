b56b14ceb228bc1c0cbba06a2030e8040f105265
ethanernst <ethanernst1@gmail.com>
2024-01-29 22:05:06 -0500

    moved vector calculation to Boid scene, changed Boid node to a RigidBody2D to use Godot built in physics
    now using apply_force in Godot for movement :+1:
    broke multiplier sliders as well 🤡 fix later

--- scenes/actors/boid/Boid.gd
+++ scenes/actors/boid/Boid.gd
@@ -1,17 +1,80 @@
-extends Node2D
+extends RigidBody2D
 
-var velocity;
-var maxSpeed = 150;
+var boids = []
+var siblings = []
+
+var max_speed = 150;
+
+var seperation = 5;   # seperation distance
+var alignment = 10;   # division factor for alignment (200 = 0.5%)
+var cohesion = 1;     # division factor for cohesion (10 = 0.5%)
+
+var seperation_multiplier : float = 1.0;
+var alignment_multiplier : float = 1.0;
+var cohesion_multiplier : float = 1.0;
 
 func _ready():
-	velocity = Vector2(randi_range(-1, 1),randi_range(-1, 1))
+	# Disable gravity and collision
+	gravity_scale = 0
+	collision_layer = 0
+	collision_mask = 0
 	
-func _process(_delta):
-	var speed = velocity.length()
-	if (speed > maxSpeed):
-		velocity = (velocity / speed) * maxSpeed
+	# Setup values
+	linear_velocity = Vector2(randi_range(-max_speed, max_speed),randi_range(-max_speed, max_speed))
 	
-	position = position + (velocity * _delta)
-	
-	var direction = velocity.normalized()
+	# Get instantiated boids and filter out self on the next frame
+	# Helps ensure all boids have been spawned
+	call_deferred("_get_other_boids")
+
+func _get_other_boids():
+	boids = get_tree().get_nodes_in_group("Boids")
+	for boid in boids:
+		if boid != self:
+			siblings.append(boid)
+
+func _process(delta):
+	var direction = linear_velocity.normalized()
 	rotation = atan2(direction.y, direction.x)
+	
+	var seperationVector: Vector2 = calcSeperation()
+	var alignmentVector: Vector2 = calcAlignment()
+	var cohesionVector: Vector2 = calcCohesion()
+	
+	var external_forces = (seperationVector + alignmentVector + cohesionVector) * delta
+	
+	apply_force(external_forces)
+
+func calcSeperation():
+	var result = Vector2(0, 0)
+	for i in siblings:
+		if ( i.position.distance_to(self.position) < (seperation * seperation_multiplier) ):
+			result -= (i.position - self.position)
+	return result
+
+func calcAlignment():
+	var percievedVelocity = Vector2(0, 0)
+	for i in siblings:
+		percievedVelocity += i.linear_velocity
+	percievedVelocity /= siblings.size()
+	
+	var result : Vector2 = (percievedVelocity - self.linear_velocity) / (alignment * alignment_multiplier)
+	return result
+
+func calcCohesion():
+	var percievedCenter = Vector2(0, 0)
+	for i in siblings:
+		percievedCenter += i.position
+	percievedCenter /= siblings.size()
+	
+	var result : Vector2  = (percievedCenter - self.position) / (cohesion * cohesion_multiplier)
+	return result
+
+# MULTIPLIER SLIDERS
+func _on_seperation_slider_value_changed(value):
+	seperation_multiplier = value
+
+func _on_alignment_slider_value_changed(value):
+	alignment_multiplier = value
+
+func _on_cohesion_slider_value_changed(value):
+	cohesion_multiplier = value
--- scenes/levels/Simulation.gd
+++ scenes/levels/Simulation.gd
@@ -1,18 +1,10 @@
 extends Node2D
 
-@export var boidCount : int = 100
-@export_range(0.5, 1.5, 1.0) var seperation_multiplier : float = 1.0;
-@export_range(0.5, 1.5, 1.0) var alignment_multiplier : float = 1.0;
-@export_range(0.5, 1.5, 1.0) var cohesion_multiplier : float = 1.0;
+@export var boidCount : int = 75
 
 var boidScene = preload("res://scenes/actors/boid/Boid.tscn")
 var viewport;
-var boids = [];
 var padding = 50;
-
-var seperation = 50; # seperation distance
-var alignment = 200;  # division factor for alignment (200 = 0.5%)
-var cohesion = 20;    # division factor for cohesion (10 = 0.5%)
 
 func _ready():
 	call_deferred("getViewportSize")
@@ -26,53 +18,8 @@ func initializeBoids():
 	for i in range(boidCount):
 		var newBoid = boidScene.instantiate()
 		newBoid.position = Vector2(randi_range(padding, viewport.x - padding),randi_range(padding, viewport.y - padding))
-		newBoid.velocity = Vector2(randi_range(padding, viewport.x - padding),randi_range(padding, viewport.y - padding))
-		boids.append(newBoid)
+		newBoid.add_to_group("Boids")
 		add_child(newBoid)
-	print("Boids: ", boids)
 
 func _process(_delta):
-	for boidInstance in boids:
-		var seperationVector: Vector2 = calcSeperation(boidInstance)
-		var alignmentVector: Vector2 = calcAlignment(boidInstance)
-		var cohesionVector: Vector2 = calcCohesion(boidInstance)
-		boidInstance.velocity += (alignmentVector + cohesionVector + seperationVector)
-
-func calcSeperation(currentBoid):
-	var result : Vector2
-	for i in boids:
-		if (i != currentBoid):
-			if ( i.position.distance_to(currentBoid.position) < (seperation * seperation_multiplier) ):
-				result -= (i.position - currentBoid.position)
-	return result
-
-func calcAlignment(currentBoid):
-	var percievedVelocity : Vector2
-	for i in boids:
-		if (i != currentBoid):
-			percievedVelocity += i.velocity
-	percievedVelocity /= (boids.size() - 1)
-	
-	var result : Vector2 = (percievedVelocity - currentBoid.velocity) / (alignment * alignment_multiplier)
-	return result
-
-func calcCohesion(currentBoid):
-	var percievedCenter : Vector2
-	for i in boids:
-		if (i != currentBoid):
-			percievedCenter += i.position
-	percievedCenter /= (boids.size() - 1)
-	
-	var result : Vector2  = (percievedCenter - currentBoid.position) / (200 * cohesion_multiplier)
-	return result
-
-# MULTIPLIER SLIDERS
-
-func _on_seperation_slider_value_changed(value):
-	seperation_multiplier = value
-
-func _on_alignment_slider_value_changed(value):
-	alignment_multiplier = value
-
-func _on_cohesion_slider_value_changed(value):
-	cohesion_multiplier = value
+	pass
